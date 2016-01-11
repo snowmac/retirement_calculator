f3adf11632468dd48398ff8afba956630298fa9c
Adam Bourg <Adam.bourg@connextions.com>
2016-01-11 10:56:15 -0700

    Added retirement accounting

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
     var retirement_income = getNumber($("input[name='retirement_income']").val());
     var current_savings = getNumber($("input[name='current_savings']").val());
     var monthly_savings = getNumber($("input[name='monthly_savings']").val());
+    var retirement_rate = getNumber($("input[name='retirement_rate']").val());
+    var max_age = getNumber($("input[name='lifespan']").val());
 
     var number_of_years = retirement_age - current_age;
     var annual_savings = monthly_savings * 12; 
@@ -34,6 +36,41 @@ $(document).ready(function(){
     $("#savings").html(numberWithCommas(monies.toFixed(2)));
     $("#cur_savings").html(numberWithCommas(current_savings));
     $("#mon_add").html(numberWithCommas(monthly_savings));
+
+
+    var saved_for_retirement = monies;
+    var retired_years = [];
+    var year = retirement_age;
+    var expected_retirement_message = null;
+
+    while(saved_for_retirement > 1) {
+      saved_for_retirement = computeInterest(
+        (saved_for_retirement-retirement_income),
+        retirement_rate,
+        12,
+        1
+      );
+
+      year++; 
+
+      retired_years.push({
+        year: year,
+        money: saved_for_retirement
+      });
+
+      if(year >= max_age) {
+        expected_retirement_message = "You will never out live your money, it will last longer then you. At "+max_age+" years old you will have: $" +  numberWithCommas(saved_for_retirement.toFixed(2));
+        break; 
+      }
+    }
+
+    if(expected_retirement_message == null){
+      expected_retirement_message = year + " years old.";
+    }
+
+    $("#retirement_growth").html(retirement_rate); 
+    $("#annual_retirement_spending").html(numberWithCommas(retirement_income.toFixed(2))); 
+    $("#age_money_runs_out").html(expected_retirement_message);
   });
 
   var computeInterest = function(principal, rate, number_of_compounded, number_of_years){
--- app/options.js
+++ app/options.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
     $("input[name='retirement_income']").val(60000);
     $("input[name='current_savings']").val(3000);
     $("input[name='monthly_savings']").val(450);
+    $("input[name='retirement_rate']").val(5);
+    $("input[name='lifespan']").val(100);
     $("#compute_button").trigger("click");
   });
   
@@ -21,6 +23,8 @@ $(document).ready(function(){
     $("input[name='retirement_income']").val(60000);
     $("input[name='current_savings']").val(1183983);
     $("input[name='monthly_savings']").val(0);
+    $("input[name='retirement_rate']").val(3);
+    $("input[name='lifespan']").val(100);
     $("#compute_button").trigger("click");
   });
 
